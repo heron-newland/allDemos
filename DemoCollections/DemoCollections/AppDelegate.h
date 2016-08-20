d8d54f36bdb407b533f45f2a339d12eeda5dcb3e
heron-newland <heron_funny@outlook.com>
2016-08-20 15:04:56 +0800

    创建仓库

---
+++ DemoCollections/DemoCollections/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoCollections
+//
+//  Created by helong on 16/8/20.
+//  Copyright © 2016年 helong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
